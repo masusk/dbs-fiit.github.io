9962bc5568dcdc2a1b549a208de12ec23e6f0975
Martin Toma <martin.toma.svk@gmail.com>
2014-12-29 15:16:18 +0100

    Fix syntax error.

--- labs/files/lab04/migration2.sql
+++ labs/files/lab04/migration2.sql
@@ -11,7 +11,7 @@ ALTER TABLE ludia ADD COLUMN titul varchar(10);
 
 -- Note: We should use BETWEEN to specify exact age ranges and not override values
 UPDATE ludia SET titul = 'Ing.' WHERE vek > 20;
-UPDATE ludia SET titul = 'doc.' WHERE vek 30;
+UPDATE ludia SET titul = 'doc.' WHERE vek > 30;
 UPDATE ludia SET titul = 'prof.' WHERE vek > 40;
 
 DELETE FROM ludia WHERE vek < 10;
