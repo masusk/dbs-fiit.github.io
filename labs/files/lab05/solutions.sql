5c802b638a40886dc424d3463e1f43b59b4f3540
Martin Toma <martin.toma.svk@gmail.com>
2014-12-30 15:15:33 +0100

    Finish new select exercises.

--- labs/files/lab05/solutions.sql
+++ labs/files/lab05/solutions.sql
@@ -2,20 +2,18 @@
 SELECT TOP 10 CreationDate FROM Users ORDER BY CreationDate asc;
 
 -- 2. Select
-
--- 3. Select
 SELECT SUM(CommentCount) FROM Posts WHERE ClosedDate > '1.1.2013'
   AND LastActivityDate < '1.1.2014';
 
--- 4. Select
+-- 3. Select
 SELECT AVG(DATEDIFF(week, CreationDate, GETDATE())) FROM Users;
 
--- 5. Select
+-- 4. Select
 SELECT AVG(DATEDIFF(day, Users.CreationDate, GETDATE())) FROM Users
 LEFT JOIN Posts ON Users.id = Posts.OwnerUserId
 WHERE Posts.id IS NULL;
 
--- 6. Select
+-- 5. Select
 SELECT TOP 10 Id FROM Users
 WHERE DATEDIFF(week, Users.CreationDate, GETDATE()) >
 (SELECT AVG(
@@ -23,16 +21,22 @@ DATEDIFF(week, Users.CreationDate, GETDATE())
 ) FROM Users)
 ORDER BY DisplayName;
 
--- 7. Select
+-- 6. Select
 SELECT AVG(pocet) FROM (SELECT COUNT(*) as pocet FROM Users u
 JOIN Posts p ON p.OwnerUserId = u.id
 GROUP BY u.id) as temp;
 
--- 8. Select
+-- 7. Select
 SELECT COUNT(*) FROM Posts p WHERE p.OwnerUserId IN
 (
 SELECT u.id FROM Users u WHERE
 u.DisplayName <> 'Community' AND u.UpVotes > 20000
 )
 
--- 9. Select
+-- 8. Select
+SELECT u.DisplayName FROM Users u LEFT JOIN Posts p on
+p.OwnerUserId = u.Id WHERE p.Score =
+(
+SELECT MAX(p.Score) FROM Posts p LEFT JOIN
+PostTypes pt ON p.PostTypeId = pt.id WHERE pt.Name = 'Answer'
+)
