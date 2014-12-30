14d0aa387c0b9c43e08e2b16b80ba04649d6b51d
Martin Toma <martin.toma.svk@gmail.com>
2014-12-30 13:54:22 +0100

    Add new select exercises.

--- labs/files/lab05/solutions.sql
+++ labs/files/lab05/solutions.sql
@@ -8,3 +8,18 @@ WHERE DATEDIFF(week, Users.CreationDate, GETDATE()) >
 DATEDIFF(week, Users.CreationDate, GETDATE())
 ) FROM Users)
 ORDER BY DisplayName;
+
+-- 6. Select
+SELECT COUNT(*) from Posts p where p.OwnerUserId IN
+(
+SELECT u.id from Users u where
+u.DisplayName <> 'Community' AND u.UpVotes > 20000
+)
+
+-- 7. Select
+SELECT SUM(CommentCount) from Posts where ClosedDate > '1.1.2013'
+  AND LastActivityDate < '1.1.2014';
+
+-- 8. Select
+
+-- 9. Select
