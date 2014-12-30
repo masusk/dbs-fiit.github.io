da6fb323b09259ea67608ff3f3c5eef7ba94695b
Martin Toma <martin.toma.svk@gmail.com>
2014-12-30 14:18:21 +0100

    Add also old questions results.

--- labs/files/lab05/solutions.sql
+++ labs/files/lab05/solutions.sql
@@ -2,9 +2,20 @@
 SELECT TOP 10 CreationDate FROM Users ORDER BY CreationDate asc;
 
 -- 2. Select
-SELECT AVG(DATEDIFF(week, CreationDate, GETDATE())) FROM Users;
+
+-- 3. Select
+SELECT SUM(CommentCount) FROM Posts WHERE ClosedDate > '1.1.2013'
+  AND LastActivityDate < '1.1.2014';
 
 -- 4. Select
+SELECT AVG(DATEDIFF(week, CreationDate, GETDATE())) FROM Users;
+
+-- 5. Select
+SELECT AVG(DATEDIFF(day, Users.CreationDate, GETDATE())) FROM Users
+LEFT JOIN Posts ON Users.id = Posts.OwnerUserId
+WHERE Posts.id IS NULL;
+
+-- 6. Select
 SELECT TOP 10 Id FROM Users
 WHERE DATEDIFF(week, Users.CreationDate, GETDATE()) >
 (SELECT AVG(
@@ -12,17 +23,16 @@ DATEDIFF(week, Users.CreationDate, GETDATE())
 ) FROM Users)
 ORDER BY DisplayName;
 
--- 6. Select
+-- 7. Select
+SELECT AVG(pocet) FROM (SELECT COUNT(*) as pocet FROM Users u
+JOIN Posts p ON p.OwnerUserId = u.id
+GROUP BY u.id) as temp;
+
+-- 8. Select
 SELECT COUNT(*) FROM Posts p WHERE p.OwnerUserId IN
 (
 SELECT u.id FROM Users u WHERE
 u.DisplayName <> 'Community' AND u.UpVotes > 20000
 )
-
--- 7. Select
-SELECT SUM(CommentCount) FROM Posts WHERE ClosedDate > '1.1.2013'
-  AND LastActivityDate < '1.1.2014';
-
--- 8. Select
 
 -- 9. Select
